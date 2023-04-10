80d638c0ad228ce9bc03fd963959bf50fd5a3b26
ferys2195 <fery21irawan.me@gmail.com>
2023-04-11 06:12:34 +0700

    add lottie animation in web fragment

--- app/src/main/java/disdukcapil/kalteng/ppid/views/fragments/WebFragment.kt
+++ app/src/main/java/disdukcapil/kalteng/ppid/views/fragments/WebFragment.kt
@@ -1,14 +1,19 @@
-package disdukcapil.kalteng.ppid
+package disdukcapil.kalteng.ppid.views.fragments
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import disdukcapil.kalteng.ppid.MainActivity
 import disdukcapil.kalteng.ppid.databinding.FragmentWebBinding
+import disdukcapil.kalteng.ppid.models.Menu
+
 
 class WebFragment : Fragment() {
     private var _binding: FragmentWebBinding? = null
@@ -20,12 +25,12 @@ class WebFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
         _binding = FragmentWebBinding.inflate(layoutInflater, container, false)
         val view = binding.root
         menu = arguments?.let { WebFragmentArgs.fromBundle(it).menu }
-        Log.d("fragmentweb", menu?.title.toString())
-        (requireActivity() as MainActivity).title = menu?.title
+
+        (activity as AppCompatActivity?)!!.supportActionBar!!.title = menu?.title
 
         menu?.url?.let {
             binding.webView.settings.javaScriptEnabled = true
@@ -33,6 +38,22 @@ class WebFragment : Fragment() {
             binding.webView.loadUrl(it)
         }
         return view
+    }
+
+    inner class WebViewClient : android.webkit.WebViewClient() {
+
+        // Load the URL
+        override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+            view.loadUrl(url)
+            return false
+        }
+
+        // ProgressBar will disappear once page is loaded
+        override fun onPageFinished(view: WebView, url: String) {
+            super.onPageFinished(view, url)
+            binding.webView.visibility = View.VISIBLE
+            binding.animationView.visibility = View.GONE
+        }
     }
 
 }
